ae05e93d737cab661c86e55371b05fdf7e11875e
pjw5521 <pjw5521@naver.com>
2021-08-02 15:51:15 +0900

    token 시간 변경

--- src/utils/TokenUtil.ts
+++ src/utils/TokenUtil.ts
@@ -16,7 +16,7 @@ export class TokenUtil {
 
         const signOptions: SignOptions = {
             algorithm: "HS256",
-            expiresIn: "1h"
+            expiresIn: 60
         }
 
         const newToken = await jwt.sign(claims, signKey, signOptions)
