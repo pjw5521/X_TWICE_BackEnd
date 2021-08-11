f82b7095e39d296facc5cd627472ce5bed886836
pjw5521 <pjw5521@naver.com>
2021-08-11 16:51:13 +0900

    vgg code 수정

--- src/controllers/vgg16.ts
+++ src/controllers/vgg16.ts
@@ -1,25 +1,38 @@
-import { Curl } from "node-libcurl"
-import { JsonController } from "routing-controllers"
+import { Curl, curly } from "node-libcurl"
+import { Get, HttpCode, JsonController, Param, Post, Res, UploadedFile } from "routing-controllers"
+import { Response } from "koa";
+import { HttpStatus } from "../types/http";
 
 @JsonController("/vgg16")
 export class Vgg16Controller{
     
-    async test(){
+    @HttpCode(HttpStatus.success)
+    @Post()
+    async uploadFile(@UploadedFile("file") file: any,@Res() { ctx }: Response){
 
         const curl = new Curl();
 
         const close = curl.close.bind(curl);
 
-        const photo_url = "https://firebasestorage.googleapis.com/v0/b/x-twice-2021.appspot.com/o/images%2Fimg1628241239744.jpg?alt=media&token=45638d12-eeb5-4370-85e9-ee33239b2abf"
-        const test_url = "http://127.0.0.1:8080/predictions/densenet161"
-        
+        const photo_file = file
+        const test_url = "http://172.16.163.153:8080/predictions/densenet161"
+    
         curl.setOpt(Curl.option.URL, test_url);
-        curl.setOpt(Curl.option.HTTPPOST, [
-            { name: 'test_input', file: photo_url }
-        ]);
+        
+        const { statusCode, data, headers } = await curly(test_url, {
+            customRequest: 'POST',
+            httpHeader: ['Content-Type: application/json'],
+            postFields: JSON.stringify({ name: 'test_input', file: photo_file })
+        })
         
         curl.on('end', close);
         curl.on('error', close);
+
+        ctx.body = {
+            data: data
+        }
+
+        return ctx;
     }
 
 }
