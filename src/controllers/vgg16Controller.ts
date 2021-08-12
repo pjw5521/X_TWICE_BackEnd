178290f361b2064fe99446459f64da91d85e9ab9
pjw5521 <pjw5521@naver.com>
2021-08-12 17:42:26 +0900

    axios 연동

--- src/controllers/vgg16Controller.ts
+++ src/controllers/vgg16Controller.ts
@@ -2,6 +2,9 @@ import { Curl, curly } from "node-libcurl"
 import { Get, HttpCode, JsonController, Param, Post, Res, UploadedFile } from "routing-controllers"
 import { Response } from "koa";
 import { HttpStatus } from "../types/http";
+import axios, {} from "axios"
+import FormData from "form-data";
+import fs from "fs"
 
 @JsonController("/vgg16")
 export class Vgg16Controller{
@@ -14,10 +17,12 @@ export class Vgg16Controller{
 
         const close = curl.close.bind(curl);
 
-        const photo_file = file
+       // const photo_file = file as File
+        const photo_file = "./kitten_small.jpg"
         const test_url = "http://172.16.163.153:8081/predictions/densenet161"
         const file_url = "https://firebasestorage.googleapis.com/v0/b/x-twice-2021.appspot.com/o/images%2Fimg1628675495434.jpg?alt=media&token=dea746e2-f8d6-48ed-b762-9f9a3936427a"
     
+        const stream = fs.createWriteStream(photo_file)
         //curl.setOpt(Curl.option.URL, test_url);
         
         /* curl.setOpt(Curl.option.HTTPPOST, [
@@ -32,10 +37,24 @@ export class Vgg16Controller{
         }) */
         
         /* const data = curl.on('end', close);
-        curl.on('error', close); */
+        curl.on('error', close); 
 
-        const data = await this.uploadImage(test_url, this.fileToBlob(photo_file));
+        const data = await this.uploadImage(test_url, "kitten_small.jpg");
         console.log(data);
+        */
+
+        console.log(file);
+
+        const formData = new FormData();
+        // formData.append('files', FileReader.readAsDataURL(file as File));
+        formData.append('file', file)
+
+        const data = await axios.post(test_url, formData, {
+            headers :{
+                'Content-Type' : 'multipart/form-data'
+            }
+        })
+        
 
         ctx.body = {
             data: data
@@ -46,17 +65,19 @@ export class Vgg16Controller{
 
 
     
-    uploadImage = (test_url: string, file: any) => new Promise((resolve, reject) => {
+    private uploadImage = (test_url: string, file: any) => new Promise((resolve, reject) => {
         const curl = new Curl();
         const close = curl.close.bind(curl);
 
         curl.setOpt(Curl.option.URL, test_url);
     
-        curl.setOpt(Curl.option.POST, file);
+        curl.setOpt(Curl.option.PUT, file);
 
         curl.on("end", (statusCode, data, headers) => {
             close();
+            console.log(statusCode);
             console.log(data);
+            console.log(headers);
             resolve(data)
         })
         curl.on("error", (error) => {
